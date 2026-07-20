75eab1b3f4c41e36508f6da314f7e124c696cbc4
Amr Gamal <amrgamal.agm@gmail.com>
2026-07-20 14:35:55 +0300

    feat: add app title

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
+import TodoForm from './TodoForm'
 
 function App() {
   return (<>
-  <h2>React ToDo</h2>
+      <h2 className="text-4xl font-bold text-heading flex items-center justify-center m-10">React ToDo</h2>
+      <TodoForm/>
   </>)
 }
 
