53a401258f7e37fc0eeded926553bf5cba2716ed
Amr Gamal <amrgamal.agm@gmail.com>
2026-07-20 19:05:01 +0300

    feat: allow TodoForm to add new todos

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import TodoForm from './TodoForm'
 import TodoList from './TodoList'
 
 function App() {
-  const [todos, setTodos] = useState([{id:1, title: 'play football', completed: false}, {id:2, title: 'eat dinner', completed: false}])
+  const [todos, setTodos] = useState([])
   function handleToggle(id){
     setTodos(todos.map((todo) => todo.id === id ? {...todo, completed: !todo.completed} : todo))
   }
+  function handleAdd(title){
+    const newTodo = {id: Date.now(), title, completed: false}
+    setTodos([newTodo, ...todos])
+  }
   return (<>
       <h2 className="text-4xl font-bold text-heading flex items-center justify-center m-10">React ToDo</h2>
-      <TodoForm/>
+      <TodoForm onAddTodo={handleAdd}/>
       <TodoList todos={todos} onToggle={handleToggle}/>
   </>)
 }
