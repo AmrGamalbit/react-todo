412a7a16b13adeb25c834d2f7831a212fa923896
Amr Gamal <amrgamal.agm@gmail.com>
2026-07-20 15:55:02 +0300

    feat: add TodoItem component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { useState } from 'react'
 import './App.css'
 import TodoForm from './TodoForm'
+import TodoItem from './TodoItem'
 
 function App() {
+  let [isChecked, setIsChecked] = useState(false)
   return (<>
       <h2 className="text-4xl font-bold text-heading flex items-center justify-center m-10">React ToDo</h2>
       <TodoForm/>
+      <TodoItem text={'Play football'} isChecked={isChecked} onToggle={()=> setIsChecked(!isChecked)}/>
   </>)
 }
 
---
+++ src/TodoItem.jsx
@@ -0,0 +1,12 @@
+import { Check } from "lucide-react"
+
+function TodoItem ({text, isChecked, onToggle}) {
+    let checkboxBg = isChecked ? 'bg-primary' : 'bg-surface'
+    return (<div className='flex justify-between bg-surface items-center w-1/2 rounded-lg py-1 px-2 mx-auto'>
+        <p className="text-body">{text}</p>
+        <div className={`w-5 h-5 border-2 border-border rounded cursor-pointer flex justify-center items-center ${checkboxBg}`} onClick={onToggle}>
+            {isChecked && <Check color="#fcfaf4" size={12} strokeWidth={3} />}
+        </div>
+    </div>)
+}
+export default TodoItem
