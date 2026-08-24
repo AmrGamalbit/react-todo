87ea1b1a29e2e6ab91721a360e880063d45aa48a
Amr Gamal <amrgamal.agm@gmail.com>
2026-08-24 08:20:20 +0300

    refactor: relocate components to dedicated components directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useRef, useState, useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
-import TodoForm from "./TodoForm";
+import TodoForm from "./components/TodoForm";
 import TodoList from "./TodoList";
-import TodoCounter from "./TodoCounter";
+import TodoCounter from "./components/TodoCounter";
 import EmptyState from "./EmptyState";
 
 function App() {
