39fc8c041a239c4b4aaeaa764490a380d47db391
Amr Gamal <amrgamal.agm@gmail.com>
2026-09-06 03:15:03 +0300

    feat: add today filter to show only today's todos

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,24 @@
 import TodoItem from "./TodoItem";
+import { useEffect, useState } from "react";
 
 function TodoList({ todos, onToggle, onEdit, onDelete, onClearCompleted }) {
+  const [showTodayOnly, setShowTodayOnly] = useState(false);
+  const visibleTodos = todos.filter((todo) => {
+    if (!showTodayOnly) return true;
+    const todayString = new Date().toLocaleDateString("en-CA");
+    return todo.dueDate === todayString;
+  });
+
   return (
     <div className="flex flex-col flex-1 gap-4 my-5 mx-auto w-1/2 h-[60vh] scrollbar-thin">
+      <button
+        className={`text-sm border border-primary w-fit ml-auto font-medium px-3 py-2 rounded-md transition-colors ${showTodayOnly ? "bg-primary text-surface  hover:bg-primary/50 " : "bg-surface text-primary hover:bg-primary/10"}`}
+        onClick={() => setShowTodayOnly((prev) => !prev)}
+      >
+        Today
+      </button>
       <div className="overflow-y-auto flex flex-col gap-2 flex-1 h-3/4">
-        {todos.map((todo) => (
+        {visibleTodos.map((todo) => (
           <TodoItem
             key={todo.id}
             todo={todo}
